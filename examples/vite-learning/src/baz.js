f78d96432b7d2e99b9a873efca07d896514a9221
Jouryjc <494208562@qq.com>
2022-03-08 22:41:10 +0800

    feat: add hmr example

--- examples/vite-learning/src/baz.js
+++ examples/vite-learning/src/baz.js
@@ -1,3 +1,9 @@
 export const sayHello = (msg) => {
-  return `Hello, ${msg}`
+  return `Hello! ${msg}`
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    console.log('baz.js updated module', newModule)
+  })
 }
--- examples/vite-learning/src/foo.js
+++ examples/vite-learning/src/foo.js
@@ -1,2 +1,28 @@
-export * from './baz'
+import { sayHello } from './baz'
+
+if (import.meta.hot) {
+  import.meta.hot.accept((newBazModule) => {
+    import.meta.hot.data = {
+      a: 1
+    }
+    console.log(
+      './baz updated module',
+      newBazModule.sayHello('hotModuleReload')
+    )
+  })
+
+  import.meta.hot.on('vite:beforeUpdate', (...args) => {
+    console.log('vite:beforeUpdate', args)
+  })
+
+  import.meta.hot.on('vite:beforeFullReload', (...args) => {
+    console.log('vite:beforeFullReload', args)
+  })
+
+  import.meta.hot.on('vite:beforePrune', (...args) => {
+    console.log('vite:beforePrune', args)
+  })
+}
+
 export const name = 'module graph'
+export { sayHello }
